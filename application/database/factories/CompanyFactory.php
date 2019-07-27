ead9cbaf94c932358c84bf581c204534d0e5fdb9
Daniel <danielgarcia992@gmail.com>
2019-07-27 16:48:27 -0500

    Factories

---
+++ application/database/factories/CompanyFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Company;
+use Faker\Generator as Faker;
+
+$factory->define(Company::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'website' => $faker->domainName
+    ];
+});
---
+++ application/database/factories/EmployeeFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Employee;
+use Faker\Generator as Faker;
+
+$factory->define(Employee::class, function (Faker $faker) {
+    return [
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'email' => $faker->unique()->safeEmail,
+        'phone' => $faker->phoneNumber
+    ];
+});
