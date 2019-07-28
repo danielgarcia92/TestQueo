6816c233d529f168c49b955f9d62b1507e31203a
Daniel <danielgarcia992@gmail.com>
2019-07-27 19:54:47 -0500

    Improve view home

--- application/resources/views/home.blade.php
+++ application/resources/views/home.blade.php
@@ -3,23 +3,16 @@
 @section('title', 'Inicio')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <div class="mx-auto w-50 card">
+        <h1 class="card-header">¿Qué desea hacer?</h1>
 
-                    You are logged in!
-                </div>
+        <div class="card-body">
+            <div class="alert alert-primary">
+                <h5><a href="{{  route('companies.index')  }}">Ver lista de Empresas</a></h5>
+                <h5><a href="{{  route('employees.index')  }}">Ver lista de Empleados</a></h5>
             </div>
         </div>
     </div>
-</div>
+
 @endsection
